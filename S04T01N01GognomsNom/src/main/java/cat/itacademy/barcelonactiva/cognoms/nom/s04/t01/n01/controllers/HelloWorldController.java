f4b7bcd7501b6875c504f64821ab9a2e8c64e58b
MarinaRueda <marina.rueda90@gmail.com>
2023-06-15 19:59:11 +0200

    Correcta

--- S04T01N01GognomsNom/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01/controllers/HelloWorldController.java
+++ S04T01N01GognomsNom/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01/controllers/HelloWorldController.java
@@ -2,23 +2,25 @@ package cat.itacademy.barcelonactiva.cognoms.nom.s04.t01.n01.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HelloWorldController {
-	
-	@GetMapping("/HelloWorld")
-	public String saluda(@RequestParam(value = "nom", defaultValue = "UNKNOWN", required = false) String nom) {
-	    return "Hola, " + (nom != null ? nom : "UNKNOWN") + ". Estàs executant un projecte Maven";
-	}
 
-	@GetMapping("/HelloWorld2/{nom}")
-	public String saluda2(@PathVariable(value = "nom", required = false) String nom) {
-	    if (nom != null) {
-	        return "Hola, " + nom + ". Estàs executant un projecte Maven";
-	    } else {
-	        return "Hola, UNKNOWN. Estàs executant un projecte Maven";
-	    }
-	}
+    @GetMapping("/HelloWorld")
+    public String saluda(@RequestParam(value = "nom", defaultValue = "UNKNOWN") String nom) {
+        return "Hola, " + nom + ". Estàs executant un projecte Maven";
+    }
+
+    @GetMapping("/HelloWorld2/{nom}")
+    public String saluda2(@PathVariable(value = "nom", required = false) String nom) {
+        if (nom != null && !nom.isEmpty()) {
+            return "Hola, " + nom + ". Estàs executant un projecte Maven";
+        } else {
+            return "Hola, UNKNOWN. Estàs executant un projecte Maven";
+        }
+    }
 }
